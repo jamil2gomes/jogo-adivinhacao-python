9e400af2246d5e62b05b547b20bbd274ed0db85a
Jamil Gomes <jamil.lannister23@gmail.com>
2019-03-05 21:24:36 -0300

    usando arquivos dentro de outros e testando se é o arquivo executavel ou não

---
+++ forca.py
@@ -0,0 +1,11 @@
+
+def jogar():
+    print("**************")
+    print("Jogo da Forca")
+    print("**************")
+
+
+
+
+if (__name__ == "__main__"): #se esse for um arquivo executavel
+    jogar()
