3e317707da2aec8a3c0b3548be70f64382dbb7f4
Jamil Gomes <jamil.lannister23@gmail.com>
2019-03-07 10:11:01 -0300

    retirei os parenteses dos ifs e do while pois não são necessários

--- adivinhacao.py
+++ adivinhacao.py
@@ -14,9 +14,9 @@ def jogar():
     print("Escolha um nível: (1) Fácil  (2) Médio  (3) Difícil")
     escolha = int(input("Escolha o nível: "))
 
-    if (escolha == 1):
+    if escolha == 1:
         total_de_tentativas = 20
-    elif (escolha == 2):
+    elif escolha == 2:
         total_de_tentativas = 10
     else:
         total_de_tentativas = 5
@@ -29,7 +29,7 @@ def jogar():
         chute = int(input("Digite o seu número: "))
         print("Você digitou ", chute)
 
-        if (chute < 1 or chute > 100):
+        if chute < 1 or chute > 100:
             print("Valor inválido. Números devem estar entre 1 e 100!")
             continue  # skip o laço
 
@@ -37,13 +37,13 @@ def jogar():
         maior = chute > numero_secreto
         menor = chute < numero_secreto
 
-        if (acertou):
+        if acertou:
             print("Parabéns! Você acertou!")
             break  # sai do laço
         else:
-            if (maior):
+            if maior:
                 print("O seu chute foi maior do que o número secreto!")
-            elif (menor):
+            elif menor:
                 print("O seu chute foi menor do que o número secreto!")
 
             pontos -= abs(numero_secreto - chute)
